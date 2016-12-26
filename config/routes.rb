193c6b1076cfe25630350bb196d0098e1fac09d5
Oluwasanmi Aderibigbe <AderibigbeOluwasanmi@gmail.com>
2016-12-26 20:06:00 +0100

    Completed chapter  12 of Lynda rails 5 essentials

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'demo#index'
-  #get 'demo/index'
-  # get ':controller(/:action(//:id))'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
