ca184911973e1828998c1a9390c765cd149558a4
Oluwasanmi Aderibigbe <AderibigbeOluwasanmi@gmail.com>
2016-12-28 15:29:47 +0100

    Completed Pages Create and New action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,9 +8,17 @@ class PagesController < ApplicationController
   end
 
   def create
+    @page = Page.new(page_params)
+    #checks if page was successfully saved to the database.
+    if @page.save
+      redirect_to(pages_path)
+    else
+      render('new')
+    end
   end
 
   def new
+    @page = Page.new
   end
 
   def delete
@@ -24,4 +32,10 @@ class PagesController < ApplicationController
 
   def edit
   end
+
+  private
+  def page_params
+    params.require(:page).permit(:id, :subject_id, :name, :permalink, :position, :visible)
+  end
+
 end
