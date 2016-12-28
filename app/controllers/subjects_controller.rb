fb316c45cc88d42bc8339c6c8bdb6a115d9ad189
Oluwasanmi Aderibigbe <AderibigbeOluwasanmi@gmail.com>
2016-12-28 09:13:20 +0100

    Completed chapter  57 of Lynda rails 5 essentials

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -5,23 +5,36 @@ class SubjectsController < ApplicationController
   end
 
   def show
+    @subject = Subject.find(params[:id])
   end
 
   def new
+    @subject = Subject.new(:name => 'Default')
   end
 
   def create
+    @subject = Subject.new(subject_params)
+    @subject.save ? redirect_to(subjects_path) : render('new')
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
   def update
+    @subject = Subject.find(params[:id])
+    @subject.update_attributes(subject_params) ? redirect_to(subject_path(@subject.id)) : render('edit')
   end
 
   def delete
   end
 
   def destroy
+  end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name, :visible, :position)
   end
 end
